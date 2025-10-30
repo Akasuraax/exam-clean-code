3ae0449025453f4ade710ccc07d48aa25183dc6c
Akas <phelina94@gmail.com>
2025-10-30 14:27:02 +0100

    test brelan

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const THROWS = [
+    [1, 1, 1, 2, 3], 
+    [2, 2, 3, 4, 5],
+    [6, 6, 6, 6, 6],
+    [1, 2, 3, 4, 5]
+]
+
 export const throwDice = () => {
     return Math.floor(Math.random() * 6) + 1;
 }
@@ -5,9 +12,25 @@ export const throwDice = () => {
 export const throwDices = (numDices) => {
     const dices = []; 
     
+    for (let i = 0; i < numDices; i++) {
+        dices.push(throwDice());
+    }
+
     return dices; 
 }
 
-export const beginYams = (numRounds) => {
+export const isBrelan = (dices) => {
+    const counts = {}; 
 
+    for (const dice of dices) {
+        counts[dice] = (counts[dice] || 0) + 1; 
+    }
+
+    for (const count of Object.values(counts)) {
+        if (count >= 3) {
+            return true; 
+        }
+    }
+
+    return false;
 }
--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { throwDice, throwDices } from ".";
+import { throwDice, throwDices, isBrelan } from ".";
 
 describe("throwDice", () => {
     it("should return a number greater than 1", () => {
@@ -15,4 +15,22 @@ describe("throwDices", () => {
     it("should return an array", () => {
         expect(Array.isArray(throwDices(5))).toBe(true);
     });
+
+    it("should return an array of the correct length", () => {
+        expect(throwDices(5).length).toBe(5);
+    })
+
+    it("should return an empty array if lower of equal than 0", () => {
+        expect(throwDices(-10).length).toBe(0);
+    })
 });
+
+describe("isBrelan", () => {
+    it("should return true for brelan", () => {
+        expect(isBrelan([1, 1, 1, 2, 3])).toBe(true);
+    })
+
+    it("should return false when it is not a brelan", () => {
+        expect(isBrelan([1, 2, 3, 4, 5])).toBe(false);
+    })
+})
